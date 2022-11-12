090abfcb7efc077532d95bc2847d7e122850363e
torinunna <torinunna9588@gmail.com>
2022-11-12 23:44:44 +0900

    implement setUpNavigationBar method to RetroVC

--- Proof/Retro/RetroViewController.swift
+++ Proof/Retro/RetroViewController.swift
@@ -5,6 +5,7 @@
 //  Created by YUJIN KWON on 2022/11/07.
 //
 
+import SnapKit
 import UIKit
 
 class RetroViewController: UIViewController {
@@ -12,7 +13,19 @@ class RetroViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        setUpNavigationBar()
+        
     }
 
 }
 
+private extension RetroViewController {
+    
+    func setUpNavigationBar() {
+        navigationItem.title = "나의 회고"
+        
+        let addButton = UIBarButtonItem(image: UIImage(systemName: "plus.circle"), style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItem = addButton
+    }
+    
+}
